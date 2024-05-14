4b2e57013212dde8deb96a16f9582f56f969e808
Dipesh <dipeshrjk27@gmail.com>
2024-05-14 23:48:57 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Music player",
+    subtitle: "HTML,CSS,JS",
+    description:
+      "This is a simple music player web app. which is mainly made in HTML, CSS and javascript. It has all the feature of a music player",
+    image: "./project-1.gif",
+    link: "#",
+  },
+  {
+    title: "Tic Tac Toe",
+    subtitle: "HTML,CSS,JS",
+    description:
+      "This is a file sharing web app made in html css js and firebase as a database.This webApp helps you to share any kind of small files",
+    image: "./project-2.gif",
+    link: "https://dipeshkrrk.github.io/tic-tac-toe/",
+  },
+  {
+    title: "EasyFileShare",
+    subtitle: "HTML,CSS,JS and Firebase",
+    description:
+      "This is a simple TIC-TAC-TOE web game which can be played offline and in a same device.",
+    image: "./project-3.gif",
+    link: "https://easyfileshare.vercel.app/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "This is a regular TODO web application, which helps you to write your todo things",
+    image: "./project-4.gif",
+    link: "#",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "MongoDB",
+  "Node js",
+];
